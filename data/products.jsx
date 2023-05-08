c3293737f48dda96a89e4d199b99db686f355db4
Jhonel Villanueva <vvjhonel@gmail.com>
2023-05-09 01:02:30 +0800

    feat: Add data for products

---
+++ data/products.jsx
@@ -0,0 +1,75 @@
+import busterSword from '../assets/images/buster-sword.png';
+import ironBlade from '../assets/images/iron-blade.png';
+import gatlingGun from '../assets/images/gatling-gun.png';
+import lightMachineGun from '../assets/images/light-machine-gun.png';
+import leatherGloves from '../assets/images/leather-gloves.png';
+import metalKnuckles from '../assets/images/metal-knuckles.png';
+import guardStick from '../assets/images/guard-stick.png';
+import arcaneScepter from '../assets/images/arcane-scepter.png';
+import fourPointShuriken from '../assets/images/4-point-shuriken.png';
+import boomerang from '../assets/images/boomerang.png';
+
+const Products = [
+  {
+    id: 1,
+    name: 'Buster Sword',
+    price: 1400,
+    image: busterSword,
+  },
+  {
+    id: 2,
+    name: 'Iron Blade',
+    price: 1200,
+    image: ironBlade,
+  },
+  {
+    id: 3,
+    name: 'Gatling Gun',
+    price: 3500,
+    image: gatlingGun,
+  },
+  {
+    id: 4,
+    name: 'Light Machine Gun',
+    price: 6000,
+    image: lightMachineGun,
+  },
+  {
+    id: 5,
+    name: 'Leather Gloves',
+    price: 100,
+    image: leatherGloves,
+  },
+  {
+    id: 6,
+    name: 'Metal Knuckles',
+    price: 1200,
+    image: metalKnuckles,
+  },
+  {
+    id: 7,
+    name: 'Guard Stick',
+    price: 220,
+    image: guardStick,
+  },
+  {
+    id: 8,
+    name: 'Arcane Scepter',
+    price: 3500,
+    image: arcaneScepter,
+  },
+  {
+    id: 9,
+    name: '4-Point Shuriken',
+    price: 2800,
+    image: fourPointShuriken,
+  },
+  {
+    id: 10,
+    name: 'Boomerang',
+    price: 1800,
+    image: boomerang,
+  },
+];
+
+export default Products;
