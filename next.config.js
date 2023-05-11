d28718d6c4eb754136088712c1f7cd1497c6a837
Jhonel Villanueva <vvjhonel@gmail.com>
2023-05-12 04:44:15 +0800

    update

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   output: 'export',
   basePath: '/odin-shopping-cart',
   assetPrefix: '/odin-shopping-cart',
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
 };
 
 module.exports = nextConfig;
