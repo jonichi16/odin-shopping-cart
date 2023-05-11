a9ed735b50f0d77363e5a42e3cb83c846dce0b33
Jhonel Villanueva <vvjhonel@gmail.com>
2023-05-12 03:46:32 +0800

    update

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,10 +6,10 @@ const Header = () => {
     <header className='bg-blue-800 shadow-md'>
       <div className='flex items-center text-slate-50 container w-11/12 mx-auto py-2 font-bold'>
         <div className='flex-1 font-serif uppercase text-2xl'>
-          <Link href='/'>Weapon Shop</Link>
+          <Link href='/odin-shopping-cart'>Weapon Shop</Link>
         </div>
         <nav className='flex gap-x-2 uppercase | md:gap-x-5'>
-          <Link href='/'>Home</Link>
+          <Link href='/odin-shopping-cart'>Home</Link>
           <Link href='/shop'>Shop</Link>
           <Link href='/shop/checkout'>Cart</Link>
         </nav>
