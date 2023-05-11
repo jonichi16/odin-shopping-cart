78cc893703a0306907cd06842740358ffb437e4c
Jhonel Villanueva <vvjhonel@gmail.com>
2023-05-11 17:26:35 +0800

    style: Add styling to loading and empty cart

--- app/loading.jsx
+++ app/loading.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const LoadingPage = () => {
   return (
-    <main className='flex-1 flex justify-center items-center'>
+    <div className='flex-1 flex justify-center items-center'>
       <div>Loading...</div>
-    </main>
+    </div>
   );
 };
 
--- components/EmptyCart.jsx
+++ components/EmptyCart.jsx
@@ -3,10 +3,12 @@ import ShopButton from './ShopButton';
 
 const EmptyCart = () => {
   return (
-    <main className='flex-1 flex flex-col justify-center items-center'>
-      <h2 className='text-center'>Your cart is empty</h2>
+    <div className='flex-1 flex flex-col justify-center items-center gap-y-5'>
+      <h2 className='text-center font-bold text-slate-500'>
+        Your cart is empty
+      </h2>
       <ShopButton />
-    </main>
+    </div>
   );
 };
 
