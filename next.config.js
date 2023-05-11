45969a3a77ea1de0175938bfab663948383e67fd
Jhonel Villanueva <vvjhonel@gmail.com>
2023-05-11 22:34:30 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  experimental: {
+    images: true,
+  },
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
+};
 
 module.exports = nextConfig;
